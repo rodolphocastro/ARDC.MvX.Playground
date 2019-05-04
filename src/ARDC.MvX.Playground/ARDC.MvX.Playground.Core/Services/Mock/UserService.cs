7f0485e035aec6f1bdb71af12273bccbfedf96fd
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-04 09:41:36 -0300

    Adicionar implementação mock do Serviço de Usuários

---
+++ src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/Services/Mock/UserService.cs
@@ -0,0 +1,48 @@
+﻿using ARDC.MvX.Playground.Core.Generators;
+using ARDC.MvX.Playground.Core.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ARDC.MvX.Playground.Core.Services.Mock
+{
+    public class UserService : IUserService
+    {
+        public User CurrentUser { get; private set; }
+
+        public bool IsAuthenticated => CurrentUser != null;
+
+        public bool ForgotPassword(string login)
+        {
+            return true;
+        }
+
+        public Task<bool> ForgotPasswordAsync(string login, CancellationToken ct)
+        {
+            return Task.FromResult(true);
+        }
+
+        public bool Login(string login, string password)
+        {
+            CurrentUser = UserGenerator.GenerateUser();
+            return true;
+        }
+
+        public Task<bool> LoginAsync(string login, string password, CancellationToken ct)
+        {
+            CurrentUser = UserGenerator.GenerateUser();
+            return Task.FromResult(true);
+        }
+
+        public bool Logout()
+        {
+            CurrentUser = null;
+            return true;
+        }
+
+        public Task<bool> LogoutAsync(CancellationToken ct)
+        {
+            CurrentUser = null;
+            return Task.FromResult(true);
+        }
+    }
+}
