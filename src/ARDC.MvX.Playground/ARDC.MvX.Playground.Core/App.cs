5fc17e010240e7b15e6b53744943a05ea4e3c4be
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-03 21:24:47 -0300

    Adicionar base do MvvMCross

---
+++ src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/App.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.ViewModels;
+
+namespace ARDC.MvX.Playground.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            // TODO: Adicionar Landing e Inicialização
+        }
+    }
+}
