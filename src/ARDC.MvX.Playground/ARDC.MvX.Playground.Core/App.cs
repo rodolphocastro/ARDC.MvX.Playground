8beb01a22361ab3a6690d5339af3f3f5e99720a9
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-04 19:11:40 -0300

    Adicionar injeção de dependência ao App

--- src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/App.cs
+++ src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/App.cs
@@ -1,5 +1,8 @@
-﻿using ARDC.MvX.Playground.Core.ViewModels.Landing;
+﻿using ARDC.MvX.Playground.Core.Services.Mock;
+using ARDC.MvX.Playground.Core.ViewModels;
+using MvvmCross.IoC;
 using MvvmCross.ViewModels;
+using System.Reflection;
 
 namespace ARDC.MvX.Playground.Core
 {
@@ -10,8 +13,14 @@ namespace ARDC.MvX.Playground.Core
     {
         public override void Initialize()
         {
-            // TODO: Adicionar Inicialização
-            RegisterAppStart<LandingViewModel>();
+            // Cadastrando todos os serviços no namespace de Mock como interfaces
+            CreatableTypes(Assembly.GetAssembly(typeof(UserService)))
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            // Definindo a primeira tela como a Home
+            RegisterAppStart<HomeViewModel>();
         }
     }
 }
