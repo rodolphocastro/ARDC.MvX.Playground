dee9c756d5e7305d77b3acbabeb5315441b57d8d
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-05 10:32:52 -0300

    Adicionar SplashScreen

---
+++ src/ARDC.MvX.Playground/Droid.Blank/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿
+using Android.App;
+using ARDC.MvX.Playground.Core;
+using MvvmCross.Droid.Support.V7.AppCompat;
+
+namespace Droid.Blank
+{
+    [Activity(Label = "@string/app_name", Theme="@style/AppTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreen : MvxSplashScreenAppCompatActivity<MvxAppCompatSetup<App>, App>
+    {
+        public SplashScreen() : base(Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
