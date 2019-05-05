18440c12fd8647291255ccf78ceaed870979acaa
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-05 12:16:58 -0300

    Adicionar View de Landing ao App

---
+++ src/ARDC.MvX.Playground/Droid.Blank/Activities/Landing.cs
@@ -0,0 +1,19 @@
+﻿
+using Android.App;
+using Android.OS;
+using ARDC.MvX.Playground.Core.ViewModels.Landing;
+using MvvmCross.Droid.Support.V7.AppCompat;
+
+namespace Droid.Blank.Activities
+{
+    [Activity(Label = "Landing")]
+    public class Landing : MvxAppCompatActivity<LandingViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Landing);
+        }
+    }
+}
