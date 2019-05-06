3938a3da6ad8d8ad7abf3351ba23deb5aede9bad
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-06 20:16:10 -0300

    Adicionar model e interface para NewsPost

---
+++ src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/Services/INewsPostService.cs
@@ -0,0 +1,41 @@
+﻿using ARDC.MvX.Playground.Core.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ARDC.MvX.Playground.Core.Services
+{
+    /// <summary>
+    /// Serviço para interação com notícias do App.
+    /// </summary>
+    public interface INewsPostService
+    {
+        /// <summary>
+        /// Obtem todas as noticias cadastradas.
+        /// </summary>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>Uma lista de notícias</returns>
+        Task<IList<NewsPost>> GetAllAsync(CancellationToken ct = default);
+
+        /// <summary>
+        /// Obtem uma notícia específica
+        /// </summary>
+        /// <param name="id">ID da noticia a ser buscada</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>A noticia encontrada</returns>
+        Task<NewsPost> GetAsync(int id, CancellationToken ct = default);
+
+        /// <summary>
+        /// Obtem todas as noticias cadastradas.
+        /// </summary>
+        /// <returns>Uma lista de notícias</returns>
+        IList<NewsPost> GetAll();
+
+        /// <summary>
+        /// Obtem uma notícia específica
+        /// </summary>
+        /// <param name="id">ID da noticia a ser buscada</param>
+        /// <returns>A noticia encontrada</returns>
+        NewsPost Get(int id);
+    }
+}
