55422e065d801e6d016e5deb5f1030dc75c1f210
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-05-07 18:34:55 -0300

    Adicionar model e interface para ServiceItem

---
+++ src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/Models/ServiceItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ARDC.MvX.Playground.Core.Models
+{
+    /// <summary>
+    /// Serviço ofertado através do App.
+    /// </summary>
+    public class ServiceItem
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool IsCobradoPorHora { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DthPublicacao { get; set; }
+
+        public DateTime? DthAtualizacao { get; set; }
+
+        public string NomeVendedor { get; set; }
+
+        public bool IsDisponivel { get; set; }
+    }
+}
---
+++ src/ARDC.MvX.Playground/ARDC.MvX.Playground.Core/Services/IServiceItemService.cs
@@ -0,0 +1,80 @@
+﻿using ARDC.MvX.Playground.Core.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ARDC.MvX.Playground.Core.Services
+{
+    /// <summary>
+    /// Serviço para interação com 
+    /// </summary>
+    public interface IServiceItemService
+    {
+        /// <summary>
+        /// Adiciona um novo serviço ao App.
+        /// </summary>
+        /// <param name="serviceItem">Serviço a ser adicionado</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        Task AddAsync(ServiceItem serviceItem = null, CancellationToken ct = default);
+
+        /// <summary>
+        /// Edita um serviço cadastrado no App.
+        /// </summary>
+        /// <param name="serviceItem">Serviço a ser editado</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        Task EditAsync(ServiceItem serviceItem, CancellationToken ct = default);
+
+        /// <summary>
+        /// Recupera todos os serviços cadastrados no App.
+        /// </summary>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>Uma lista contendo is serviços encontrados</returns>
+        Task<IList<ServiceItem>> GetAllAsync(CancellationToken ct = default);
+
+        /// <summary>
+        /// Recupera um serviço específico no App.
+        /// </summary>
+        /// <param name="id">ID do serviço a ser buscada</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>O serviço encontrado</returns>
+        Task<ServiceItem> GetAsync(int id, CancellationToken ct = default);
+
+        /// <summary>
+        /// Remove um serviço do App.
+        /// </summary>
+        /// <param name="id">ID do serviço a ser removido</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        void RemoveAsync(int id, CancellationToken ct = default);
+
+        /// <summary>
+        /// Adiciona um novo serviço ao App.
+        /// </summary>
+        /// <param name="serviceItem">Serviço a ser adicionado</param>
+        void Add(ServiceItem serviceItem = null);
+
+        /// <summary>
+        /// Edita um serviço cadastrado no App.
+        /// </summary>
+        /// <param name="serviceItem">Serviço a ser editado</param>
+        void Edit(ServiceItem serviceItem);
+
+        /// <summary>
+        /// Recupera todos os serviços cadastrados no App.
+        /// </summary>
+        /// <returns>Uma lista contendo is serviços encontrados</returns>
+        IList<ServiceItem> GetAll();
+
+        /// <summary>
+        /// Recupera um serviço específico no App.
+        /// </summary>
+        /// <param name="id">ID do serviço a ser buscada</param>
+        /// <returns>O serviço encontrado</returns>
+        ServiceItem Get(int id);
+
+        /// <summary>
+        /// Remove um serviço do App.
+        /// </summary>
+        /// <param name="id">ID do serviço a ser removido</param>
+        void Remove(int id);
+    }
+}
