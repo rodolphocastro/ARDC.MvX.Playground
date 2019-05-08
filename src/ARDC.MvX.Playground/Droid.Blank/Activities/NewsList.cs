695709344033b7d55eb49f01394f19e50f630b2f
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-05-08 17:48:06 -0300

    Adicionar View para lista de Notícias

---
+++ src/ARDC.MvX.Playground/Droid.Blank/Activities/NewsList.cs
@@ -0,0 +1,19 @@
+﻿
+using Android.App;
+using Android.OS;
+using ARDC.MvX.Playground.Core.ViewModels.News;
+using MvvmCross.Droid.Support.V7.AppCompat;
+
+namespace Droid.Blank.Activities
+{
+    [Activity(Label = "Notícias")]
+    public class NewsList : MvxAppCompatActivity<NewsListViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.NewsList);
+        }
+    }
+}
